1c7ac19a613da3021c79240acb38dc7f271acf22
justin daly <justin.m.daly@gmail.com>
2023-11-29 21:46:39 +0700

    add claptrap include

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,21 +6,23 @@
 /*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 16:14:21 by jdaly             #+#    #+#             */
-/*   Updated: 2023/11/29 16:24:18 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/11/29 21:39:16 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 
 int main()
 {
-    ScavTrap scavTrap1("Scavvy1");
-    ScavTrap scavTrap2("Scavvy2");
-
-    scavTrap1.attack("the wall");
-    scavTrap1.takeDamage(10);
-    scavTrap1.beRepaired(10);
-    scavTrap1.guardGate();
+    ScavTrap scavtrap1("Scavvy1");
+    ScavTrap scavtrap2("Scavvy2");
+    ScavTrap scavtrap3 = scavtrap1;
+    
+    scavtrap1.attack("the target");
+    scavtrap2.takeDamage(500);
+    scavtrap2.beRepaired(10);
+    scavtrap2.guardGate();
 
     return (0);
 
