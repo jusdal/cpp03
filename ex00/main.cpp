793a0cb454ae6e035ca32d2ec170e9814d961a7e
justin daly <justin.m.daly@gmail.com>
2023-11-30 16:15:26 +0700

    correct main attack message

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: justindaly <justindaly@student.42.fr>      +#+  +:+       +#+        */
+/*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 20:31:12 by jdaly             #+#    #+#             */
-/*   Updated: 2023/11/30 15:00:28 by justindaly       ###   ########.fr       */
+/*   Updated: 2023/11/30 15:57:44 by jdaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ int main(void)
     ClapTrap    claptrap3(claptrap2);
     ClapTrap    claptrap4("Robot");
 
-    claptrap1.attack("Victim");		//victim loses 1 hit point/attacker loses 1 energy point
+    claptrap1.attack("target");		//attacker loses 1 energy point
     claptrap2.takeDamage(20);		//victim loses 20 hitPoints (goes to 0)
     claptrap2.beRepaired(10); 		//victim not able to repair becuase no hitPoints
 
-    claptrap2.attack("Robot");		//not enough hitPoints to attack
+    claptrap2.attack("target");		//not enough hitPoints to attack
     claptrap4.takeDamage(5); 		//loses 5 hitPoints (goes to 5)
     claptrap4.beRepaired(400);		//hitPoints go to max (10)
     claptrap4.takeDamage(1000);		//hitPoints go to 0
